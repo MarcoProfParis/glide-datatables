ced10b1d1a2c9007700cac0f445b764277b50899
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2022-11-03 18:56:20 +0100

    Update function.js

--- function.js
+++ function.js
@@ -65,14 +65,44 @@ tr.group:hover {
   <script type="text/javascript" class="init">
     $(document).ready(function () {
     var groupColumn = 1;
-      $('#${tableID.value}').DataTable({
-        colReorder: true,
-        dom: '${dom}',
-        buttons: [
-            ${buttonSpec}
-        ]
-      });
-    });
+    	var table = $('tableID.value').DataTable({
+		columnDefs: [{ visible: false, targets: groupColumn }],
+		order: [[groupColumn, 'asc']],
+		displayLength: 25,
+		dom: '${dom}',
+                buttons: [
+                ${buttonSpec}
+                ],
+		drawCallback: function (settings) {
+			var api = this.api();
+			var rows = api.rows({ page: 'current' }).nodes();
+			var last = null;
+
+			api
+				.column(groupColumn, { page: 'current' })
+				.data()
+				.each(function (group, i) {
+					if (last !== group) {
+						$(rows)
+							.eq(i)
+							.before('<tr class="group"><td colspan="5">' + group + '</td></tr>');
+
+						last = group;
+					}
+				});
+		},
+	});
+
+	// Order by the grouping
+	$('#tableID.value tbody').on('click', 'tr.group', function () {
+		var currentOrder = table.order()[0];
+		if (currentOrder[0] === groupColumn && currentOrder[1] === 'asc') {
+			table.order([groupColumn, 'desc']).draw();
+		} else {
+			table.order([groupColumn, 'asc']).draw();
+		}
+	});
+});
   </script>
   `;
 
