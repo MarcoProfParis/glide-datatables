a94ef0e5a4082dbcb2cb51df9cfccc0194ebd9c0
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2023-01-29 16:50:34 +0100

    Update functionorigin.js

--- functionorigin.js
+++ functionorigin.js
@@ -49,6 +49,7 @@ window.function = function (table, tableID, buttons, colReorder, domOptions) {
     $(document).ready(function () {
       $('#${tableID.value}').DataTable({
         colReorder: true,
+        language: {url: 'dataTables.german.json'},
         dom: '${dom}',
         buttons: [
             ${buttonSpec}
