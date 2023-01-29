21cd4e8a539514b19725fbd1dedf2e02b10e11d3
Marc Olivier Reula <113138450+MarcoProfParis@users.noreply.github.com>
2023-01-29 16:52:34 +0100

    Update functionorigin.js

--- functionorigin.js
+++ functionorigin.js
@@ -49,6 +49,7 @@ window.function = function (table, tableID, buttons, colReorder, domOptions) {
     $(document).ready(function () {
       $('#${tableID.value}').DataTable({
         colReorder: true,
+        "pagingType": "scrolling",
         language: {url: 'dataTables.french.json'},
         dom: '${dom}',
         buttons: [
